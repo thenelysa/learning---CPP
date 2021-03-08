8363405a20f1bdebe8853dea3eeba41efdefe016
Neelisha Shrestha <80264725+thenelysa@users.noreply.github.com>
2021-03-08 23:01:13 +0545

    First code in CPP
    This program prints names.

---
+++ print-name.cpp
@@ -0,0 +1,16 @@
+// my first program
+#include <iostream>
+using namespace std;
+int main() {
+/*main program
+*/	
+	cout<< "Neelisha Shrestha"<< endl ;
+	cout<< "Kshitiz Rai"<< endl;
+	cout<< "Coco Rai Shrestha"<< endl;
+	
+	
+	
+
+	return 0;
+}
+//End of program 
