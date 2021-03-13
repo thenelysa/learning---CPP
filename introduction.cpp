29184391c1ceb0df80a365053bee5f0840af4e50
Neelisha Shrestha <80264725+thenelysa@users.noreply.github.com>
2021-03-13 21:18:01 +0545

    Intro

---
+++ introduction.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int age = 19;
+const float height = 5.7;
+	double salary = 50000;
+	char gender = 'F';
+	bool ismarried = false ;
+	string name ="Neelisha Shrestha";
+	
+	//Print 
+	
+	cout<<"Name = "<<name<<endl;
+	cout<<" Age = "<<age<<"years old"<<endl;
+	cout<<" Height = "<<height<<endl;	
+	cout<<" Gender = "<<gender<<endl;
+	cout<<" Is married? = "<<ismarried<<endl;
+	cout<<" Monthly salary = "<<salary<<endl;
+	
+	
+	return 0;
+	
+	
+	
+	
+	
+}
