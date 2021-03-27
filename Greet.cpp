185d69fcfca80783a3fdef241c1bb41482da7585
Neelisha Shrestha <80264725+thenelysa@users.noreply.github.com>
2021-03-27 17:27:22 +0545

    :)

---
+++ Greet.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+ using namespace std;
+ int main(){
+ 	
+ 	string name ;
+ 	 cout << " Enter name : ";
+ 	 cin >> name;
+ 	 
+ 	 cout << " Hello " << name << " . You are doing great ! ";
+ 	 
+ 	 return 0;
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ }
