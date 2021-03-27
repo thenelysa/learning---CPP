fc432b317d78bc2c21bee8caa3c2b1fe41f00ff0
Neelisha Shrestha <80264725+thenelysa@users.noreply.github.com>
2021-03-27 17:13:09 +0545

    voter & non voter method

---
+++ Election.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+/* syntax for if 
+if(condition)*/
+
+int age;
+ cout <<"Enter age : "<<endl;
+ cin>> age ;
+ 
+	if (age >18){
+		
+		cout << "Voter ";
+		
+	}else{
+		
+		cout <<"Not voter";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
